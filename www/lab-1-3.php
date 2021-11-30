e0818ef82cc093569a8559ceec5a9959f0b2a675
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-01 02:17:45 +0500

    тип перменной на булевый

--- www/lab-1-3.php
+++ www/lab-1-3.php
@@ -2,6 +2,6 @@
   define('NUM_E', 2.71828);
   print ('Число е равно ' . NUM_E . '<br>');
   $num_e1=NUM_E;
-  settype($num_e1, string);
+  settype($num_e1, boolean);
   print ('$num_e1=' . $num_e1 . ' - ' . gettype($num_e1));
 ?>
