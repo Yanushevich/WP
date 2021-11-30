8d0d207ec1723c66504eca87a7b9653eb7485cc0
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-01 02:15:48 +0500

    изменение типа переменной на строковый

--- www/lab-1-3.php
+++ www/lab-1-3.php
@@ -2,5 +2,6 @@
   define('NUM_E', 2.71828);
   print ('Число е равно ' . NUM_E . '<br>');
   $num_e1=NUM_E;
+  settype($num_e1, string);
   print ('$num_e1=' . $num_e1 . ' - ' . gettype($num_e1));
 ?>
