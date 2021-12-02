fb777be10194fa9a28c7a2af53663e86eb9f5782
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-02 14:54:09 +0500

    $a=false; $b="false";

--- www/lab-2-1.php
+++ www/lab-2-1.php
@@ -1,6 +1,6 @@
 <title> Янушевич Максим ПИ-322 </title>
 <?php
-$a=10; $b="true";
+$a=false; $b="false";
 if ($a) { echo '$a='."$a - истина<br>"; }
  else { echo '$a='."$a - ложь<br>"; }
 if ($b) { echo '$b='."$b - истина<br>"; }
