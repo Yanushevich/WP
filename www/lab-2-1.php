72f41f7e62ec0509514439959c58405836e682e1
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-02 14:53:30 +0500

    $a=0; $b="0";

---
+++ www/lab-2-1.php
@@ -0,0 +1,8 @@
+<title> Янушевич Максим ПИ-322 </title>
+<?php
+$a=0; $b = "0";
+if ($a) { echo '$a='."$a - истина<br>"; }
+ else { echo '$a='."$a - ложь<br>"; }
+if ($b) { echo '$b='."$b - истина<br>"; }
+ else { echo '$b='."$b - ложь<br>"; }
+?>
