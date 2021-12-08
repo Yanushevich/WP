a34166700fede5b3e7471ab077f0996da6c4c08e
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-08 18:18:29 +0500

    исправление ошибок

--- lab2/lab-2-5.php
+++ lab2/lab-2-5.php
@@ -1,8 +1,12 @@
 <title> Янушевич Максим ПИ-322</title>
 <?php
   
-  $a=round((rand(10,20)/rand(1,5)-4),1);
-  $b=round((rand(10,20)/rand(1,5)-4),1);
+  $a=round((rand(10,20)/rand(1,10)),1);
+  $b=round((rand(10,20)/rand(1,10)),1);
+  if ($a==$b){ 
+    $b-=0.2;
+  } 
+
   print ('a=' . $a . '<br> b=' . $b);
 
   function ut($u, $t) {
