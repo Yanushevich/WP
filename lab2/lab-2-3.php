317676fe33061030a2ba68a9e7e38c0ac14c7e11
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-08 17:07:35 +0500

    ассоциативный массив

---
+++ lab2/lab-2-3.php
@@ -0,0 +1,13 @@
+<title> Янушевич Максим ПИ-322</title>
+<?php
+  
+  $arr=array("cnum" => 2001,
+	     "cname" => "Hoffman",
+	     "city" => "London",
+	     "snum" => 1001);
+  foreach($arr as $key => $value)
+  {
+     echo "$key = $value <br />";
+  }
+  
+?>
