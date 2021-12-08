26c2b7049641b8e509b9595bf5f0c794fb128c07
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-08 14:15:55 +0500

    Массив квадратных чисел

--- lab2/lab-2-1.php
+++ lab2/lab-2-1.php
@@ -1,9 +1,18 @@
 <title> Янушевич Максим ПИ-322</title>
 <?php
   $treug=array();
+  $kvd=array();
+
   for ($n = 1; $n <= 10; $n++)
   {
     $treug[$n]=$n*($n+1)/2;
     print ($treug[$n] . " ");
   }
+    print ("<br>");
+  for ($n = 1; $n <= 10; $n++)
+  {
+    $kvd[$n]=$n*$n;
+    print ($kvd[$n] . " ");
+  }
+
 ?>
