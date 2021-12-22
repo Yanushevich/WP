1e2ec04a4b9fcdeb8f1759f06d18773be6838f7d
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-23 01:46:29 +0500

    Задача 3.4

---
+++ lab3/s3-4.php
@@ -0,0 +1,25 @@
+<title> Янушевич Максим ПИ-322</title>
+<body>
+<h3> Задача 3.4 </h3>
+<p>Проверка регистрации:
+<form method="POST" action="<?php print $PHP_SELF ?>">
+ <p>Логин: <INPUT type="text" name="n">
+ <P> <INPUT type="submit" name="obr" value="Проверить">
+</form>
+
+<?
+$n=$_POST["n"];
+$arr=array();
+$arr[0]="Иванов Иван Иванович";
+$arr[1]="Петров Петр Петрович";
+$arr[2]="Сидоров Лаврентий Геннадьевич";
+$arr[3]="Шолохов Николай Григорьевич";
+if (isset($_POST["obr"])) {
+
+  for ($i = 0; $i < count($arr); $i++) {
+    if ($n == $arr[$i]) { echo 'Здравствуйте, ' . $n; $acc=false; break;}
+    else { $acc=true; }
+  }
+  if ($acc) { echo 'Вы не зарегистрированный пользователь!';}
+
+}
