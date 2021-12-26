351686c79327d35897f7a77c96be27079588618e
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-27 02:52:51 +0500

    3 лаба, задача 3.6

--- lab3/s3-6.php
+++ lab3/s3-6.php
@@ -1,50 +1,54 @@
 <title> Янушевич Максим ПИ-322</title>
+
 <?
-$text1 = $_POST["text1"];
-$text2 = $_POST["text2"];
-$text3 = $_POST["text3"];
-$sym1 = $_POST["sym1"];
-$sym2 = $_POST["sym2"];
-$let = $_POST["letter"];
-
-$s1c = 0;
-$s2c = 0;
-$letc = 0;
-$rev = "";
-$t1 = array();
-$t2 = array();
-$t3 = array();
-
-if (isset($_POST["text1"])) {
-
-  echo '<h3> Вариант 13 </h3>';
+if (isset($_POST["f1"])) {
+  $text1 = $_POST["text1"];
+  $t1 = array();
+  $let = $_POST["letter"];
+  $letc = 0;
+  echo '<h3> Вариант 3 </h3>';
   echo $text1;
   echo '<br>';
-  $t1 = preg_split('//u', $text1, -1, PREG_SPLIT_NO_EMPTY);
-  foreach ($t1 as $z) {
-    if ($sym1 == $z) { $s1c++; }
-    if ($sym2 == $z) { $s2c++; }
-  }
-  echo ('Символ "' . $sym1 . '" встречается ' . $s1c . ' раз <br>');
-  echo ('Символ "' . $sym2 . '" встречается ' . $s2c . ' раз');
-
-  echo '<h3> Вариант 3 </h3>';
-  echo $text2;
-  echo '<br>';
-  $t2 = explode (' ', $text2);
-  foreach ($t2 as $str) {
+  $t1 = explode (' ', $text1);
+  foreach ($t1 as $str) {
     if (mb_substr($str, 0, 1,"utf-8") == $let) { $letc++; }
   }
   echo ('Число слов, начинающихся на букву ' . $let . ': ' . $letc . '<br>');
+}
 
-  echo '<h3> Вариант 16 </h3>';
+if (isset($_POST["f2"])) {
+  $text2 = $_POST["text2"];
+  $t2 = array();
+  $count = (int)$_POST["count"];
+  echo '<h3> Вариант 7 </h3>';
+  echo $text2;
+  echo '<br> Слова, имеющие число букв ' . $count . ': <br>';
+  $t2 = explode(' ', $text2);
+
+  foreach ($t2 as $str) {
+    if (iconv_strlen($str, "utf-8") == $count) { echo ($str . '<br>');}
+  }
+} 
+
+if (isset($_POST["f3"])) {
+  $text3 = $_POST["text3"];
+  $let1 = $_POST["let1"];
+  $let2 = $_POST["let2"];
+  $t3 = array();
+  echo '<h3> Вариант 11 </h3>';
   echo $text3;
   echo '<br>';
-  $t3 = explode (' ', $text3);
-  $rev = array_reverse($t3);
-  echo ('Строка с обратным порядком слов: ');
-  foreach ($rev as $z1) {
-    echo ($z1 . ' ');
+  $t3 = preg_split('//u', $text3, -1, PREG_SPLIT_NO_EMPTY);
+  foreach ($t3 as $i => $v) {
+    if (mb_substr($t3[$i], 0, 1,"utf-8") == $let1) {
+      if(mb_substr($t3[$i+1], 0, 1,"utf-8") == $let2) {
+	unset($t3[$i]);
+      }
+    }
+  }
+  echo ('Обработанный текст: ');
+  for ($i = 0; $i < count($t3); $i++) {
+    echo $t3[$i];
   }
 
 }
