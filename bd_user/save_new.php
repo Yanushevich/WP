715d6f36b6699456c01239a204e1344f21ab642a
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-26 19:57:31 +0500

    Исправление ошибок с кодировкой

--- bd_user/save_new.php
+++ bd_user/save_new.php
@@ -6,10 +6,10 @@
  mysqli_select_db($conn, "heroku_60a6c74570f65ce") or die("Нет такой таблицы!");
  // Строка запроса на добавление записи в таблицу:
  
- $sql_add = "INSERT INTO user SET user_name='" . iconv(mb_detect_encoding($_GET['name'], mb_detect_order(), true), "cp1251", $_GET['name'])
-."', user_login='".iconv(mb_detect_encoding($_GET['login'], "utf-8"), "cp1251", $_GET['login'])."', user_password='"
-.iconv(mb_detect_encoding($_GET['password'], "utf-8"), "cp1251", $_GET['password'])."', user_e_mail='".iconv(mb_detect_encoding($_GET['e_mail'], "utf-8"), "cp1251", $_GET['e_mail']).
-"', user_info='".iconv(mb_detect_encoding($_GET['info'], "utf-8"), "cp1251", $_GET['info']). "'";
+ $sql_add = "INSERT INTO user SET user_name='" . iconv("utf-8", "cp1251", $_GET['name'])
+."', user_login='".iconv("utf-8", "cp1251", $_GET['login'])."', user_password='"
+.iconv("utf-8", "cp1251", $_GET['password'])."', user_e_mail='".iconv("utf-8", "cp1251", $_GET['e_mail']).
+"', user_info='".iconv("utf-8", "cp1251", $_GET['info']). "'";
  mysqli_query($conn,$sql_add); // Выполнение запроса
  if (mysqli_affected_rows($conn)>0) // если нет ошибок при выполнении запроса
  { print "<p>Спасибо, вы зарегистрированы в базе данных.";
