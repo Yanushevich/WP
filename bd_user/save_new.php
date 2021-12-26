ad3afaed38b0853f8683ff04a8d409c69e43bfce
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-26 19:42:03 +0500

    Исправление ошибок с кодировкой

--- bd_user/save_new.php
+++ bd_user/save_new.php
@@ -1,16 +1,17 @@
 <?php
  // Подключение к базе данных:
- mysql_connect("eu-cdbr-west-02.cleardb.net", "b94b976f849e9f", "f60a7bfb") or die ("Невозможно
+ $conn=mysqli_connect("eu-cdbr-west-02.cleardb.net", "b94b976f849e9f", "f60a7bfb") or die ("Невозможно
 подключиться к серверу");
- mysql_query('SET NAMES cp1251'); // Тип кодировки
- mysql_select_db("heroku_60a6c74570f65ce") or die("Нет такой таблицы!");
+ mysqli_query($conn,'SET NAMES cp1251'); // Тип кодировки
+ mysqli_select_db($conn, "heroku_60a6c74570f65ce") or die("Нет такой таблицы!");
  // Строка запроса на добавление записи в таблицу:
- $sql_add = "INSERT INTO user SET user_name='" . $_GET['name']
-."', user_login='".$_GET['login']."', user_password='"
-.$_GET['password']."', user_e_mail='".$_GET['e_mail'].
-"', user_info='".$_GET['info']. "'";
- mysql_query($sql_add); // Выполнение запроса
- if (mysql_affected_rows()>0) // если нет ошибок при выполнении запроса
+ 
+ $sql_add = "INSERT INTO user SET user_name='" . iconv(mb_detect_encoding($_GET['name'], mb_detect_order(), true), "cp1251", $_GET['name'])
+."', user_login='".iconv(mb_detect_encoding($_GET['login'], mb_detect_order(), true), "cp1251", $_GET['login'])."', user_password='"
+.iconv(mb_detect_encoding($_GET['password'], mb_detect_order(), true), "cp1251", $_GET['password'])."', user_e_mail='".iconv(mb_detect_encoding($_GET['e_mail'], mb_detect_order(), true), "cp1251", $_GET['e_mail']).
+"', user_info='".iconv(mb_detect_encoding($_GET['info'], mb_detect_order(), true), "cp1251", $_GET['info']). "'";
+ mysqli_query($conn,$sql_add); // Выполнение запроса
+ if (mysqli_affected_rows($conn)>0) // если нет ошибок при выполнении запроса
  { print "<p>Спасибо, вы зарегистрированы в базе данных.";
  print "<p><a href=\"index.php\"> Вернуться к списку
 пользователей </a>"; }
