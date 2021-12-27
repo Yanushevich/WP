3bd6e839375ad150dde8fa43ba8cfce684708c81
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-28 00:02:49 +0500

    Выгрузка в Excel

--- bd_banks/index.php
+++ bd_banks/index.php
@@ -81,6 +81,6 @@ print("<P>Всего вкладов: $num_rows </p>");
 <p> <a href="new_vkl.php"> Добавить вклад </a>
 
 <p> <a href="inf_pdf.php"> Выгрузить данные в PDF </a>
-
+<p> <a href="inf_xls.php"> Выгрузить данные в Excel </a>
 
 </body> </html>
