913b9198764a0b84b80e630e36bdb5cce2ea0dde
Yanushevich <maksim_yanushevich@mail.ru>
2021-12-28 22:43:46 +0500

    Исправление ошибок

--- bd_banks/edit_user.php
+++ bd_banks/edit_user.php
@@ -30,8 +30,8 @@ if (iconv("cp1251", "utf-8", $st['type']) == 1) { echo 'Оператор</option
 elseif (iconv("cp1251", "utf-8", $st['type']) == 2) { echo 'Администратор</option>'; }
 }
 echo '</select>';
-echo "<br>Имя пользователя: <input name='username' size='20' value='".$username."' type='text'>";
-echo "<br>Пароль: <input name='password' size='20' maxlength='32' value='".$password."' type='text'>";
+echo "<br>Имя пользователя: <input name='username' size='20' type='text'>";
+echo "<br>Пароль: <input name='password' size='20' maxlength='32' type='text'>";
 ?>
 
 <br>Тип: <select name='type'>
